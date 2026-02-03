7c8c95a1d700a699d8edd88092d5d888de12505c
febreeze <sayhello@hvlmnns.de>
2026-02-03 21:34:10 +0100

    fix: update URL to point to nvim repo

--- nvim.lua
+++ nvim.lua
@@ -1,5 +1,5 @@
 -- README_START
-local URL = "https://raw.githubusercontent.com/yesitsfebreeze/dotfiles/refs/heads/master/nvim.lua"
+local URL = "https://raw.githubusercontent.com/yesitsfebreeze/nvim/refs/heads/master/nvim.lua"
 local d = vim.fn.stdpath("config").."/lua"
 vim.fn.mkdir(d, "p") 
 if not (vim.uv or vim.loop).fs_stat(d .. "/nvim.lua") then vim.fn.system({"curl","-fsSL",URL,"-o", d .. "/nvim.lua"}) end
